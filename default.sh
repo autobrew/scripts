ad634f0e22eb4a47d135ebb3d41c4839a07af410
Jeroen Ooms <jeroenooms@gmail.com>
2017-03-16 22:37:42 +0100

    also delete dependency dynlibs

--- default.sh
+++ default.sh
@@ -7,4 +7,4 @@ curl -fsSL https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C
 BREW_DEPS=$($BREW deps -n $PKG_BREW_NAME)
 HOMEBREW_CACHE="/tmp" $BREW install --force-bottle $BREW_DEPS $PKG_BREW_NAME 2>&1 | perl -pe 's/Warning/Note/gi'
 rm -f $BREWDIR/Cellar/$PKG_BREW_NAME/*/lib/*.dylib
-rm -f $BREWDIR/opt/$PKG_BREW_NAME/lib/*.dylib
+rm -f $BREWDIR/opt/*/lib/*.dylib
