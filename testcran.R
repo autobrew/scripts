11ea0073c344ad4bce1c03e42c5b77985f0ff284
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-20 16:33:22 +0100

    Install binary packages first

--- testcran.R
+++ testcran.R
@@ -1,4 +1,26 @@
 url <- 'https://api.github.com/search/code?q=autobrew+in:file+filename:configure+user:cran'
 repos <- jsonlite::fromJSON(url)
-pkgs <- sort(repos$items$repository$name)
-install.packages(pkgs, type = 'source')
+pkgs <- sort(unique(repos$items$repository$name))
+print(pkgs)
+
+# Install binary packages + dependencies
+install.packages(pkgs, type = "binary")
+
+# Install packages from source
+results <- sapply(pkgs, function(pkg){
+  try(remove.packages(pkg), silent = TRUE)
+  install.packages(pkg, type = 'source')
+  tryCatch({
+    find.package(pkg)
+    TRUE
+  }, error = function(e){
+    FALSE
+  })
+})
+
+# Check if everything is installed
+if(any(!results)){
+	stop("Packages failed to install:", names(which(results)))
+}
+print("Great success!")
+print(pkgs)
