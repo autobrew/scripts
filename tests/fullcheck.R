24f16dca31117d2a935a9f0909dab95a43e803d0
Jeroen Ooms <jeroenooms@gmail.com>
2021-03-20 14:01:37 +0100

    CI updates

--- tests/fullcheck.R
+++ tests/fullcheck.R
@@ -5,6 +5,13 @@ options(repos = 'https://cloud.r-project.org')
 install.packages("gh")
 res <- gh::gh('/search/code?per_page=100&q=autobrew+in:file+filename:configure+user:cran')
 pkgs <- vapply(res$items, function(x){x$repository$name}, "")
+
+# Extra packages (need to source setupgdal.sh first)
+if(nchar(Sys.getenv("PROJ_GDAL_DATA_COPY"))){
+  pkgs <- c('sf', 'rgdal', pkgs)
+}
+
+# All the packages
 pkgs <- sort(unique(pkgs))
 print(pkgs)
 
@@ -16,20 +23,13 @@ print(pkgs)
 dir.create(pkgdir <- tempfile())
 download.packages(pkgs, pkgdir)
 
-# Extra packages (need to source setupgdal.sh first)
-if(nchar(Sys.getenv("PROJ_GDAL_DATA_COPY"))){
-  pkgs <- c('sf', 'rgdal', pkgs)
-  download.packages(c('sf', 'rgdal'), pkgdir, repos = c("https://r-spatial.r-universe.dev", "https://r-forge.universe.dev"))
-}
-
 # Install all packages + dependencies
 # Note depends=TRUE omits LinkingTo for binary packages
 deps <- tools::package_dependencies(pkgs, which = c("Depends", "Imports", "LinkingTo", "Suggests"))
 pkgdeps <- sort(unique(unlist(deps, use.names = FALSE)))
 install.packages(pkgdeps, type = "binary")
 
-
-Sys.setenv("_R_CHECK_FORCE_SUGGESTS_"=0)
+#Sys.setenv("_R_CHECK_FORCE_SUGGESTS_"=0)
 tools::check_packages_in_dir(pkgdir, check_args = '--no-manual --no-build-vignettes')
 
 # Get outputs
